16f71291053d2626e2a042d2183ee26984c77dc0
Max Anderson MOUMOUNI <happydev.229@gmail.com>
2022-12-20 21:51:14 +0100

    Setup Intl

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const appConfig = require('./app.config');
+const path = require('path');
+const {
+    I18n
+} = require('i18n');
+const i18n = new I18n({
+    locales: ['en', 'fr'],
+    directory: path.join(__dirname, 'locales')
+})
 
 const fs = require('fs');
 const {
@@ -19,8 +27,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cookieParser());
-app.use(express.static(__dirname + "/client/public"))
-app.set("views", "./client/views/")
+app.use(express.static(path.join(__dirname, "client/public")))
+app.set("views", path.join(__dirname, "client/views/"))
 app.set("view engine", "ejs")
 
 
@@ -31,8 +39,19 @@ app.listen(appConfig.port, (err) => {
         log(`Server on http://localhost:${appConfig.port}`);
 })
 
-app.get("/", (req, res) => {
+app.get("/:lang", (req, res) => {
+    if (req.params.lang != "en" && req.params.lang != "fr") return res.redirect("/" + localeLang());
+    i18n.init(req, res);
+    res.setLocale(req.params.lang);
     res.render('index');
-
 })
 
+app.get("/", (req, res) => {
+    
+    res.redirect("/" + localeLang());
+})
+
+function localeLang() {
+    let locale = Intl.DateTimeFormat().resolvedOptions().locale;
+    return locale.split("-")[0];
+}
